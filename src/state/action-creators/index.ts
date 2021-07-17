e459f3376953b87ddabbbb9de9f5cf463da1f7c6
Talita Azevedo <talita.azevedo360@gmail.com>
2021-07-17 15:18:55 -0300

    s04 Reminder on Event types

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { ActionType } from '../action-types';
 import { Action } from '../actions';
 import { Dispatch } from 'redux';
-const searchRepositories = (term: string) => {
+export const searchRepositories = (term: string) => {
   return async (dispatch: Dispatch<Action>) => {
     dispatch({
       type: ActionType.SEARCH_REPOSITORIES,
